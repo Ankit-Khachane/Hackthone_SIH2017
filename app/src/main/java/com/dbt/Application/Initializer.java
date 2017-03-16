478bc1c0df5a6c4e2704b9df150af9c09f472828
Ankit Khachane <khachane.ankit417@gmail.com>
2017-03-16 23:25:54 +0530

    Configured App Initializer & Manifests.

--- app/src/main/java/com/dbt/Application/Initializer.java
+++ app/src/main/java/com/dbt/Application/Initializer.java
@@ -2,6 +2,10 @@ package com.dbt.Application;
 
 import android.app.Application;
 
+import com.dbt.R;
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+
 /**
  * Created by admin on 3/16/2017.
  */
@@ -10,6 +14,13 @@ public class Initializer extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        TODO:Oadd parse configeration code.
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getResources().getString(R.string.ApplicationId))
+                .clientKey(getResources().getString(R.string.AndroidClientKey))
+                .server(getResources().getString(R.string.ServerAddress))
+                .build());
+
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+        installation.saveInBackground();
     }
 }
