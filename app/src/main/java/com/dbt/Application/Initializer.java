5af408ffa4cf739f50f96074fb2d7c510bd994ae
Ankit <khachane.ankit417@gmail.com>
2017-03-16 16:07:49 +0530

    Initial commit DBT Android Application.

---
+++ app/src/main/java/com/dbt/Application/Initializer.java
@@ -0,0 +1,15 @@
+package com.dbt.Application;
+
+import android.app.Application;
+
+/**
+ * Created by admin on 3/16/2017.
+ */
+
+public class Initializer extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        TODO:Oadd parse configeration code.
+    }
+}
---
+++ app/src/main/java/com/dbt/Splash.java
@@ -0,0 +1,14 @@
+package com.dbt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        //TODO: add bounce animation splash screen.
+    }
+}
