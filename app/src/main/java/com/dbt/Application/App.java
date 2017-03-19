7e91de3d05361463f0785a3327d1293918376070
Ankit Khachane <khachane.ankit417@gmail.com>
2017-03-19 20:23:14 +0530

    Added Splash Screen code for Animating logo.

---
+++ app/src/main/java/com/dbt/Application/App.java
@@ -0,0 +1,10 @@
+package com.dbt.Application;
+
+/**
+ * Created by khach on 19-03-2017.
+ */
+
+public class App {
+
+
+}
--- app/src/main/java/com/dbt/Splash.java
+++ app/src/main/java/com/dbt/Splash.java
@@ -1,14 +1,62 @@
 package com.dbt;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Splash extends AppCompatActivity {
+    String TAG = getClass().getSimpleName();
+    ImageView logo;
+    Animation anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
         //TODO: add bounce animation splash screen.
+        logo = (ImageView) findViewById(R.id.dbt_logo);
+        anim = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+        if (isNetworkAvailable()) {
+            Toast.makeText(this, "Internet is Available", Toast.LENGTH_SHORT).show();
+            //Splash Screen Thread Logic
+            anim.setDuration(2000);
+            anim.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    Toast.makeText(Splash.this, "Animation Started", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    Toast.makeText(Splash.this, "Animation Stopped", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+            logo.startAnimation(anim);
+        } else {
+            Toast.makeText(this, "No Internet Connection.", Toast.LENGTH_SHORT).show();
+            //exit if no internet connection is available
+        }
+        Log.i(TAG, "onCreate: isInternetConnected -" + isNetworkAvailable());
+    }
+
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
